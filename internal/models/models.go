7dd2d631920060f4d8fb2bbdb0acd1513d427e05
diazjohan98 <vasquez.johan@correounivalle.edu.co>
2026-04-14 07:34:50 -0500

    feat: crear modelo de usuario y ejecutar automigración
    - Se creó la estructura User para soportar contraseñas cifradas
    - Se agregó el modelo a AutoMigrate de GORM para generar las tablas
    - Se insertó el primer administrador manualmente vía terminal

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,22 +9,24 @@ import (
 
 type DBModels struct {
 	Order OrderModels
+	User  UserModel
 }
 
 func InitDB(dataSourceName string) (*DBModels, error) {
 	db, err := gorm.Open(sqlite.Open(dataSourceName), &gorm.Config{})
 
 	if err != nil {
-		return nil, fmt.Errorf("Failed to migrate database: %v", err)
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
 	}
 
-	err = db.AutoMigrate(&Order{}, &OrderItem{})
+	err = db.AutoMigrate(&Order{}, &OrderItem{}, &User{})
 	if err != nil {
-		return nil, fmt.Errorf("Failed to migrate database %v", err)
+		return nil, fmt.Errorf("failed to migrate database: %v", err)
 	}
 
 	dbModel := &DBModels{
 		Order: OrderModels{DB: db},
+		User:  UserModel{DB: db},
 	}
 	return dbModel, nil
 }
